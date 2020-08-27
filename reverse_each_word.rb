60f5ef016f5d40d368fd8e7ca9800b12d9601baf
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-27 01:09:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,4 @@ def reverse_each_word(greeting)
   greeting.split.collect{|c| c.reverse}.join(" ")
 end
 
-puts reverse_each_word("Hi again, just making sure it's reversed!")
+#puts reverse_each_word("Hi again, just making sure it's reversed!")
